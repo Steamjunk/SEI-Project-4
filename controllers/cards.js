020bad0cbc9cb18f6f958e7eb5532b015754d946
Chris Myers <myerschristopher@johndeere.com>
2020-11-16 15:46:24 -0600

    cleanup

--- controllers/cards.js
+++ controllers/cards.js
@@ -447,7 +447,6 @@ const sdkWhereStatement = (params) => {
     if(params.subtype !== 'Any') {
         whereStatement.subtypes = params.subtype
     }
-    console.log(whereStatement)
     return whereStatement
 }
 
